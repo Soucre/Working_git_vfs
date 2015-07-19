641b99b3c4b2a2392c5405f3029398102d599fae
AnhCopNe <n.hieu86@gmail.com>
2015-07-19 22:01:38 +0700

    upload miss, upload again

--- tools.vfs.com.vn/vfs_div.sql
+++ tools.vfs.com.vn/vfs_div.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE [dbo].VFS_DIV
 GO
 
 CREATE PROCEDURE [dbo].VFS_DIV
-@YearReport int = 2014 -- test default
+@YearReport int -- test default
 AS 
 
 ---------------------------------------------------------
@@ -18,15 +18,19 @@ BEGIN
 		DIV2011 float,
 		DIV2010 float,
 		CoTucKH2014 float,
-		CoTucKH2015 float
+		CoTucKH2015 float,
+		DoanhThuKeHoach float,
+		LoiNhuanKeHoach float,
+		DoanhThuLyKe float,
+		LoiNhuanSauThue float
 	)
 	-- insert the current price into table 
 	INSERT INTO #TABLETEMP 
-		SELECT RTRIM(LTRIM(StockSymbol)) as StockSymbol, Last*10 , 0,0,0,0,0,0,0
+		SELECT RTRIM(LTRIM(StockSymbol)) as StockSymbol, Last*10 , 0,0,0,0,0,0,0,0,0,0,0
 		FROM [StoxData].[dbo].[stox_tb_HOSE_Trading]  
 		where DateReport in (select top 1 DateReport from stox_tb_HOSE_Trading order by DateReport DESC) 
 		union 
-		select code, close_price ,0,0,0,0,0,0,0
+		select code, close_price ,0,0,0,0,0,0,0,0,0,0,0
 		FROM [StoxData].[dbo].[stox_tb_StocksInfo] 
 		where trading_date in ( select top 1 trading_date from stox_tb_StocksInfo order by trading_date DESC) 
 		
@@ -66,19 +70,30 @@ BEGIN
 	FROM (SELECT [Ticker],[Cotuccuanam] CoTuc2015 FROM [stox_tb_corp_DividendYield] WHERE [YearReport] = @YearReport + 1) TableTemp
 	WHERE TableTemp.Ticker = #TABLETEMP.Stock
 
+	-- doanh thu và loi nhuan ke hoach
+	UPDATE #TABLETEMP SET  DoanhThuKeHoach = TableTemp.DoanhThu, LoiNhuanKeHoach =  LoiNhanTruocThue
+	FROM (select [Ticker],DoanhThu DoanhThu, LoiNhuanTruocThue LoiNhanTruocThue FROM stox_tb_fund_BCTC_KeHoach WHERE YEAR(UpdateDate)  = @YearReport + 1) TableTemp
+	WHERE TableTemp.Ticker = #TABLETEMP.Stock
 
+	-- doanh thu và loi nhuan truoc thue
+	UPDATE #TABLETEMP SET  DoanhThuLyKe = TableTemp.DoanhThu, LoiNhuanKeHoach =  TableTemp.LNST
+	FROM (select [Ticker],SUM(F2_92) DoanhThu, SUM(F2_106) LNST FROM [stox_tb_fund_CompanyIncomeStatement] WHERE YearReport  = @YearReport + 1 AND LengthReport in (1,2,3,4) GROUP BY [Ticker]) TableTemp
+	WHERE TableTemp.Ticker = #TABLETEMP.Stock
+	
 
 	Select Top 20
-		Stock ,
-		Price ,
-		DIV2014 ,
-		DIV2013 ,
-		DIV2012 ,
-		DIV2011 ,
-		DIV2010 ,
+		Stock,
+		Price,
+		round(DIV2014,1) DIV2014,
+		round(DIV2013,1) DIV2013,
+		round(DIV2012,1) DIV2012,
+		round(DIV2011,1) DIV2011,
+		round(DIV2010,1) DIV2010,
 		round(((CoTucKH2014 - (DIV2014 * 100)) / Price) * 100 , 1) AS Nam2014ChuaChia  ,
 		round(CoTucKH2015 / 100   ,1) AS Nam2015KeHoach,
-		round((((CoTucKH2014 - (DIV2014 * 100)) / Price) * 100 ) + (CoTucKH2015 / 100) , 1) AS TongCong
+		round((((CoTucKH2014 - (DIV2014 * 100)) / Price) * 100 ) + (CoTucKH2015 / 100) , 1) AS TongCong,
+		round((CASE WHEN DoanhThuKeHoach = 0 THEN 0 ELSE DoanhThuLyKe/DoanhThuKeHoach END) * 100 ,1) as DoanhThuKeHoach
+		
 
 	from #TABLETEMP
 	order by TongCong DESC
