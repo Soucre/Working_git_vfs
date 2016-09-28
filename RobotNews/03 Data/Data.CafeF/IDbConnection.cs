8b9ecdf37f106964681744565b0a9de8269b6c2c
Nguyen Chi Hieu <hieu.nguyen@vfs.com.vn>
2016-09-28 21:03:49 +0700

    merge

---
+++ RobotNews/03 Data/Data.CafeF/IDbConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Data.CafeF
+{
+    public class IDbConnections
+    {
+        static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServerConnString"].ConnectionString);
+    }
+}
